b101f4b64f746fc2713cf47b86260c914d430f3e
Dr-Charlous <113095075+Dr-Charlous@users.noreply.github.com>
2024-04-26 20:22:59 +0200

    a revoir quoi

--- Assets/Scripts/PositionsCube.cs
+++ Assets/Scripts/PositionsCube.cs
@@ -16,71 +16,71 @@ public class PositionsCube : MonoBehaviour
     private void OnMouseDown()
     {
 
-        Done.Clear();
+        //Done.Clear();
 
-        if (FindPath(Player.GetComponent<Player>().positionsCube, this))
-        {
-            List<PositionsCube> path = GetPath(this);
+        //if (FindPath(Player.GetComponent<Player>().positionsCube, this))
+        //{
+        //    List<PositionsCube> path = GetPath(this);
 
-            Vector3 from = Position + Offset;
+        //    Vector3 from = Position + Offset;
 
-            for (int i = 0; i < path.Count; i++)
-            {
-                PositionsCube tile = path[i];
+        //    for (int i = 0; i < path.Count; i++)
+        //    {
+        //        PositionsCube tile = path[i];
 
-                Vector3 destination = tile.transform.position + tile.Offset;
-                Player.transform.position = destination;
-            }
+        //        Vector3 destination = tile.transform.position + tile.Offset;
+        //        Player.transform.position = destination;
+        //    }
 
-            MoveEnd();
+        //    MoveEnd();
 
-            foreach (var tile in path)
-            {
-                tile.Previous = null;
-            }
-        }
+        //    foreach (var tile in path)
+        //    {
+        //        tile.Previous = null;
+        //    }
+        //}
 
         Player.transform.position = transform.position + Offset;
     }
 
-    void MoveEnd()
-    {
-        Player.GetComponent<Player>().positionsCube = this;
-    }
+    //void MoveEnd()
+    //{
+    //    Player.GetComponent<Player>().positionsCube = this;
+    //}
 
-    bool FindPath(PositionsCube start, PositionsCube end)
-    {
-        if (start == end)
-            return true;
+    //bool FindPath(PositionsCube start, PositionsCube end)
+    //{
+    //    if (start == end)
+    //        return true;
 
-        Done.Add(start);
+    //    Done.Add(start);
 
-        foreach (var item in start.Neighbors)
-        {
-            if (Done.Contains(item) == false && FindPath(item, end))
-            {
-                item.Previous = start;
+    //    foreach (var item in start.Neighbors)
+    //    {
+    //        if (Done.Contains(item) == false && FindPath(item, end))
+    //        {
+    //            item.Previous = start;
 
-                return true;
-            }
-        }
+    //            return true;
+    //        }
+    //    }
 
-        return false;
-    }
+    //    return false;
+    //}
 
-    List<PositionsCube> GetPath(PositionsCube lastTile)
-    {
-        List<PositionsCube> path = new List<PositionsCube>();
+    //List<PositionsCube> GetPath(PositionsCube lastTile)
+    //{
+    //    List<PositionsCube> path = new List<PositionsCube>();
 
-        PositionsCube current = lastTile;
-        while (current != null)
-        {
-            path.Insert(0, current);
-            current = current.Previous;
-        }
+    //    PositionsCube current = lastTile;
+    //    while (current != null)
+    //    {
+    //        path.Insert(0, current);
+    //        current = current.Previous;
+    //    }
 
-        path.RemoveAt(0);
+    //    path.RemoveAt(0);
 
-        return path;
-    }
+    //    return path;
+    //}
 }
